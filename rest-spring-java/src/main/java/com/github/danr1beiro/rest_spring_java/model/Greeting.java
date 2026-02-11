c46ed61d33537ee6f67ff96a2df2f501ecc2b673
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-11 13:33:08 -0300

    feat: Initial project structure and greeting endpoint

---
+++ rest-spring-java/src/main/java/com/github/danr1beiro/rest_spring_java/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.github.danr1beiro.rest_spring_java.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ rest-spring-java/src/test/java/com/github/danr1beiro/rest_spring_java/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.danr1beiro.rest_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
