4d72b48bb36bc429fe1510472bf26591311a834b
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-12 12:03:45 -0300

    refactor: Change method annotation from @RequestMapping to @GetMapping

--- rest-spring-java/src/main/java/com/github/danr1beiro/rest_spring_java/controller/GreetingController.java
+++ rest-spring-java/src/main/java/com/github/danr1beiro/rest_spring_java/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.github.danr1beiro.rest_spring_java.controller;
 
 import com.github.danr1beiro.rest_spring_java.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
